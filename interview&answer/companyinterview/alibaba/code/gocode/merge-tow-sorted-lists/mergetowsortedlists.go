0666536c3193197bf1e36bc06f89c35d779afe09
admin <panzhongsheng>
2023-05-18 14:21:29 +0800

    update

---
+++ interview&answer/companyinterview/alibaba/code/gocode/merge-tow-sorted-lists/mergetowsortedlists.go
@@ -0,0 +1,59 @@
+/*
+@File   : mergetowsortedlists.go
+@Author : pan
+@Time   : 2023-05-18 13:51:48
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var first, firstone, firsttwo ListNode1
+	first.Val = 1
+	firstone.Val = 3
+	firsttwo.Val = 5
+	first.Next = &firstone
+	firstone.Next = &firsttwo
+
+	var second, secondone, secondtwo ListNode1
+	second.Val = 2
+	secondone.Val = 4
+	secondtwo.Val = 6
+	second.Next = &secondone
+	secondone.Next = &secondtwo
+
+	var node *ListNode1
+	node = mergeTwoLists1(&first, &second)
+
+	for {
+		fmt.Print(node.Val, "->")
+		node = node.Next
+		if node.Next == nil {
+			fmt.Println(node.Val)
+			break
+		}
+	}
+}
+
+type ListNode1 struct {
+	Val  int
+	Next *ListNode1
+}
+
+// 合并两个有序链表
+func mergeTwoLists1(l1 *ListNode1, l2 *ListNode1) *ListNode1 {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists1(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists1(l1, l2.Next)
+		return l2
+	}
+}
---
+++ interview&answer/companyinterview/alibaba/code/gocode/threesum/threesum.go
@@ -0,0 +1,52 @@
+/*
+@File   : threesum.go
+@Author : pan
+@Time   : 2023-05-18 13:58:48
+*/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+}
+
+// 三数之和
+func threeSum(nums []int) [][]int {
+	res := make([][]int, 0)
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		target := 0 - nums[i]
+		left := i + 1
+		right := len(nums) - 1
+		if nums[i] > 0 || nums[i]+nums[left] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for left < right {
+			if left > i+1 && nums[left] == nums[left-1] {
+				left++
+				continue
+			}
+			if right < len(nums)-2 && nums[right] == nums[right+1] {
+				right--
+				continue
+			}
+			if nums[left]+nums[right] > target {
+				right--
+			} else if nums[left]+nums[right] < target {
+				left++
+			} else {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+			}
+		}
+	}
+	return res
+}
