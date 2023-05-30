4618fa25da1769eb860c8335d007aa341c76b508
pzspsh <3277440871@qq.com>
2023-05-30 10:49:43 +0800

    update

---
+++ interview&answer/companyinterview/bytedance/code/gocode/buildtree/buildtree.go
@@ -0,0 +1,34 @@
+/*
+@File   : buildtree.go
+@Author : pan
+@Time   : 2023-05-29 22:25:38
+*/
+package main
+
+import "fmt"
+
+func main() {
+	res := buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})
+	fmt.Println(res)
+	fmt.Println(res.Val, res.Left.Val, res.Right.Val, res.Right.Left.Val, res.Right.Right.Val)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 从前序与中序遍历序列构造二叉树
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	for k := range inorder {
+		if inorder[k] == preorder[0] {
+			return &TreeNode{
+				Val:   preorder[0],
+				Left:  buildTree(preorder[1:k+1], inorder[0:k]),
+				Right: buildTree(preorder[k+1:], inorder[k+1:]),
+			}
+		}
+	}
+	return nil
+}
