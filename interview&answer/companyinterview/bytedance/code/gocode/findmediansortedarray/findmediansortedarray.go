4cb0efd1d6162fe84dc4349cd9b54100ae283f2f
pzspsh <3277440871@qq.com>
2023-05-30 10:49:43 +0800

    update

---
+++ interview&answer/companyinterview/bytedance/code/gocode/findmediansortedarray/findmediansortedarray.go
@@ -0,0 +1,42 @@
+/*
+@File   : findmediansortedarray.go
+@Author : pan
+@Time   : 2023-05-22 09:56:19
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(findMedianSortedArrays2([]int{1, 2}, []int{3, 4}))
+}
+
+// 寻找两个正序数组的中位数
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	total := len(nums1) + len(nums2)
+	a, b := total/2, (total-1)/2
+	count := 0
+	res := 0
+	for i, j := 0, 0; i < len(nums1) || j < len(nums2); count++ {
+		if i < len(nums1) && (j == len(nums2) || nums1[i] < nums2[j]) {
+			if count == a {
+				res = res + nums1[i]
+			}
+			if count == b {
+				res = res + nums1[i]
+			}
+			i++
+		} else {
+			if count == a {
+				res = res + nums2[j]
+			}
+			if count == b {
+				res = res + nums2[j]
+			}
+			j++
+		}
+	}
+	return float64(res) / 2
+}
