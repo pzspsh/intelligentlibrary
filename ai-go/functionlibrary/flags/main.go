4cc0fe6b1a0bdaf149573a5f5990d2ff5cc68d46
pzspsh <3277440871@qq.com>
2023-06-06 23:05:09 +0800

    update

--- ai-go/functionlibrary/flags/main.go
+++ ai-go/functionlibrary/flags/main.go
@@ -5,6 +5,41 @@
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"function/flags/flags"
+)
+
+type Options struct {
+	name     string
+	Email    flags.StringSlice
+	Phone    string
+	Address  flags.StringSlice
+	fileSize flags.Size
+}
+
+func main() {
+	testOptions := &Options{}
+	CheckUpdate := func() {
+		fmt.Println("checking if new version is available")
+		fmt.Println("updating tool....")
+	}
+
+	flagSet := flags.NewFlagSet()
+	flagSet.CreateGroup("info", "Info",
+		flagSet.StringVarP(&testOptions.name, "name", "n", "", "name of the user"),
+		flagSet.StringSliceVarP(&testOptions.Email, "email", "e", nil, "email of the user", goflags.CommaSeparatedStringSliceOptions),
+	)
+	flagSet.CreateGroup("additional", "Additional",
+		flagSet.StringVarP(&testOptions.Phone, "phone", "ph", "", "phone of the user"),
+		flagSet.StringSliceVarP(&testOptions.Address, "address", "add", nil, "address of the user", goflags.StringSliceOptions),
+		flagSet.CallbackVarP(CheckUpdate, "update", "ut", "update this tool to latest version"),
+		flagSet.SizeVarP(&testOptions.fileSize, "max-size", "ms", "", "max file size"),
+	)
+
+	if err := flagSet.Parse(); err != nil {
+		log.Fatal(err)
+	}
 }
--- ai-go/functionlibrary/go.mod
+++ ai-go/functionlibrary/go.mod
@@ -11,11 +11,14 @@ require (
 )
 
 require (
+	github.com/cnf/structhash v0.0.0-20201127153200-e1b16c1ebc08 // indirect
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
