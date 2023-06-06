f6a9786dda0e51a3363f71e20694f586367c775b
admin <panzhongsheng>
2023-06-06 17:39:02 +0800

    update

---
+++ ai-go/functionlibrary/flags/main.go
@@ -0,0 +1,10 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2023-06-06 17:35:36
+*/
+package main
+
+func main() {
+
+}
---
+++ ai-py/python3tutorial/examples/readme.md
@@ -0,0 +1,6 @@
+# 例题(example)
+### 1、终端开启http.server服务
+```python
+例题：
+>python -m http.server --bind 0.0.0.0 8080
+```
