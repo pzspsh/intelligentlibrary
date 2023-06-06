026065f20ea71a6e4b10615460c56e77f27529df
pzspsh <3277440871@qq.com>
2023-06-06 23:05:09 +0800

    update

---
+++ ai-go/functionlibrary/flags/flags/slice_common.go
@@ -0,0 +1,154 @@
+package goflags
+
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+var quotes = []rune{'"', '\'', '`'}
+
+func isQuote(char rune) (bool, rune) {
+	for _, quote := range quotes {
+		if quote == char {
+			return true, quote
+		}
+	}
+	return false, 0
+}
+
+func searchPart(value string, stop rune) (bool, string) {
+	var result string
+	for _, char := range value {
+		if char != stop {
+			result += string(char)
+		} else {
+			return true, result
+		}
+	}
+	return false, result
+}
+
+func ToString(slice []string) string {
+	defaultBuilder := &strings.Builder{}
+	defaultBuilder.WriteString("[")
+	for i, k := range slice {
+		defaultBuilder.WriteString("\"")
+		defaultBuilder.WriteString(k)
+		defaultBuilder.WriteString("\"")
+		if i != len(slice)-1 {
+			defaultBuilder.WriteString(", ")
+		}
+	}
+	defaultBuilder.WriteString("]")
+	return defaultBuilder.String()
+}
+
+type Options struct {
+	// IsFromFile determines if the values are from file
+	IsFromFile func(string) bool
+	// IsEmpty determines if the values are empty
+	IsEmpty func(string) bool
+	// Normalize the value (eg. removing trailing spaces)
+	Normalize func(string) string
+	// IsRaw determines if the value should be considered as a raw string
+	IsRaw func(string) bool
+}
+
+// ToStringSlice converts a value to string slice based on options
+func ToStringSlice(value string, options Options) ([]string, error) {
+	var result []string
+	if options.IsEmpty == nil && options.IsFromFile == nil && options.Normalize == nil {
+		return []string{value}, nil
+	}
+
+	addPartToResult := func(part string) {
+		if !options.IsEmpty(part) {
+			if options.Normalize != nil {
+				part = options.Normalize(part)
+			}
+			result = append(result, part)
+		}
+	}
+	if FileExists(value) && options.IsFromFile != nil && options.IsFromFile(value) {
+		linesChan, err := ReadFile(value)
+		if err != nil {
+			return nil, err
+		}
+		for line := range linesChan {
+			addPartToResult(line)
+		}
+	} else if options.IsRaw != nil && options.IsRaw(value) {
+		addPartToResult(value)
+	} else {
+		index := 0
+		for index < len(value) {
+			char := rune(value[index])
+			if isQuote, quote := isQuote(char); isQuote {
+				quoteFound, part := searchPart(value[index+1:], quote)
+
+				if !quoteFound {
+					return nil, errors.New("Unclosed quote in path")
+				}
+
+				index += len(part) + 2
+
+				addPartToResult(part)
+			} else {
+				commaFound, part := searchPart(value[index:], ',')
+
+				if commaFound {
+					index += len(part) + 1
+				} else {
+					index += len(part)
+				}
+
+				addPartToResult(part)
+			}
+		}
+	}
+	return result, nil
+}
+
+func ReadFile(filename string) (chan string, error) {
+	if !FileExists(filename) {
+		return nil, errors.New("file doesn't exist")
+	}
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		f, err := os.Open(filename)
+		if err != nil {
+			return
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+	}()
+
+	return out, nil
+}
+
+func isEmpty(s string) bool {
+	return strings.TrimSpace(s) == ""
+}
+
+func isFromFile(_ string) bool {
+	return true
+}
+
+func normalizeTrailingParts(s string) string {
+	return strings.TrimSpace(s)
+}
+
+func normalize(s string) string {
+	return strings.TrimSpace(strings.Trim(strings.TrimSpace(s), string(quotes)))
+}
+
+func normalizeLowercase(s string) string {
+	return strings.TrimSpace(strings.Trim(strings.TrimSpace(strings.ToLower(s)), string(quotes)))
+}
---
+++ ai-go/functionlibrary/flags/flags/string_slice.go
@@ -0,0 +1,53 @@
+package goflags
+
+var (
+	optionMap           map[*StringSlice]Options
+	optionDefaultValues map[*StringSlice][]string
+)
+
+func init() {
+	optionMap = make(map[*StringSlice]Options)
+	optionDefaultValues = make(map[*StringSlice][]string)
+}
+
+// StringSlice is a slice of strings
+type StringSlice []string
+
+// Set appends a value to the string slice.
+func (stringSlice *StringSlice) Set(value string) error {
+	option, ok := optionMap[stringSlice]
+	if !ok {
+		option = StringSliceOptions
+	}
+	values, err := ToStringSlice(value, option)
+	if err != nil {
+		return err
+	}
+	// if new values are provided, we remove default ones
+	if defaultValue, ok := optionDefaultValues[stringSlice]; ok {
+		if Equal(*stringSlice, defaultValue) {
+			*stringSlice = []string{}
+		}
+	}
+
+	*stringSlice = append(*stringSlice, values...)
+	return nil
+}
+
+func (stringSlice StringSlice) String() string {
+	return ToString(stringSlice)
+}
+
+func Equal[T comparable](s1, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	for idx := range s1 {
+		if s1[idx] != s2[idx] {
+			return false
+		}
+	}
+
+	return true
+}
