aa380e72713ae15eb0f0ecdd48b3cdd574042a30
admin <panzhongsheng>
2023-06-07 14:29:49 +0800

    update

---
+++ ai-go/framelearn/projects/pangin/cmd/main.go
@@ -0,0 +1,18 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2023-06-07 14:27:58
+*/
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ ai-go/framelearn/projects/pangin/pkg/api/router.go
@@ -0,0 +1,6 @@
+/*
+@File   : router.go
+@Author : pan
+@Time   : 2023-06-07 14:27:43
+*/
+package api
