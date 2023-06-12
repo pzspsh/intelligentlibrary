f4781d6b1d9a4be8b5933aecb0c9cc25de4fb1f1
admin <panzhongsheng>
2023-06-12 11:07:37 +0800

    update

---
+++ apve/pkg/models/models.go
@@ -0,0 +1,6 @@
+/*
+@File   : models.go
+@Author : pan
+@Time   : 2023-06-12 11:03:09
+*/
+package models
--- apve/server/server.go
+++
@@ -1,6 +0,0 @@
-/*
-@Time   : 2023-04-27 10:41:55
-@Author : pan
-@File   : server.go
-*/
-package server
