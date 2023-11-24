00ea8a5793a64dafd7693f1e821daf5741fc7b97
admin <panzhongsheng>
2023-11-24 15:16:11 +0800

    update

---
+++ ai-go/gotutorial/examples/goweb/routing/main.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2023-11-24 15:03:27
+*/
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":80", r)
+}
--- ai-go/gotutorial/go.mod
+++ ai-go/gotutorial/go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
