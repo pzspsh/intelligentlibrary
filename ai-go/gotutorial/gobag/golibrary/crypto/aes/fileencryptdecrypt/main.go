dda21eb22cd057bce49885cce13b730b81a0c1ad
admin <panzhongsheng>
2023-11-29 18:02:03 +0800

    update

--- ai-go/gotutorial/gobag/golibrary/crypto/aes/fileencryptdecrypt/main.go
+++ ai-go/gotutorial/gobag/golibrary/crypto/aes/fileencryptdecrypt/main.go
@@ -1,17 +1,20 @@
 /*
 @File   : main.go
 @Author : pan
-@Time   : 2023-11-29 17:51:43
+@Time   : 2023-11-29 17:59:40
 */
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"os"
+	"time"
 )
 
 //加密过程：
@@ -105,14 +108,106 @@ func DecryptByAes(data string) ([]byte, error) {
 	return AesDecrypt(dataByte, PwdKey)
 }
 
+// 更新 文件 的加解密
+
+// EncryptFile 文件加密，filePath 需要加密的文件路径 ，fName加密后文件名
+func EncryptFile(filePath, fName string) (err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("未找到文件")
+		return
+	}
+	defer f.Close()
+
+	fInfo, _ := f.Stat()
+	fLen := fInfo.Size()
+	fmt.Println("待处理文件大小:", fLen)
+	maxLen := 1024 * 1024 * 100 //100mb  每 100mb 进行加密一次
+	var forNum int64 = 0
+	getLen := fLen
+
+	if fLen > int64(maxLen) {
+		getLen = int64(maxLen)
+		forNum = fLen / int64(maxLen)
+		fmt.Println("需要加密次数：", forNum+1)
+	}
+	//加密后存储的文件
+	ff, err := os.OpenFile("encryptFile_"+fName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("文件写入错误")
+		return err
+	}
+	defer ff.Close()
+	//循环加密，并写入文件
+	for i := 0; i < int(forNum+1); i++ {
+		a := make([]byte, getLen)
+		n, err := f.Read(a)
+		if err != nil {
+			fmt.Println("文件读取错误")
+			return err
+		}
+		getByte, err := EncryptByAes(a[:n])
+		if err != nil {
+			fmt.Println("加密错误")
+			return err
+		}
+		//换行处理，有点乱了，想到更好的再改
+		getBytes := append([]byte(getByte), []byte("\n")...)
+		//写入
+		buf := bufio.NewWriter(ff)
+		buf.WriteString(string(getBytes[:]))
+		buf.Flush()
+	}
+	ffInfo, _ := ff.Stat()
+	fmt.Printf("文件加密成功，生成文件名为：%s，文件大小为：%v Byte \n", ffInfo.Name(), ffInfo.Size())
+	return nil
+}
+
+// DecryptFile 文件解密
+func DecryptFile(filePath, fName string) (err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("未找到文件")
+		return
+	}
+	defer f.Close()
+	fInfo, _ := f.Stat()
+	fmt.Println("待处理文件大小:", fInfo.Size())
+
+	br := bufio.NewReader(f)
+	ff, err := os.OpenFile("decryptFile_"+fName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("文件写入错误")
+		return err
+	}
+	defer ff.Close()
+	num := 0
+	//逐行读取密文，进行解密，写入文件
+	for {
+		num = num + 1
+		a, err := br.ReadString('\n')
+		if err != nil {
+			break
+		}
+		getByte, err := DecryptByAes(a)
+		if err != nil {
+			fmt.Println("解密错误")
+			return err
+		}
+
+		buf := bufio.NewWriter(ff)
+		buf.Write(getByte)
+		buf.Flush()
+	}
+	fmt.Println("解密次数：", num)
+	ffInfo, _ := ff.Stat()
+	fmt.Printf("文件解密成功，生成文件名为：%s，文件大小为：%v Byte \n", ffInfo.Name(), ffInfo.Size())
+	return
+}
+
 func main() {
-	data := []byte("adfasdbdfsdfsdfs")
-	//加密
-	str, _ := EncryptByAes(data)
-	//解密
-	str1, _ := DecryptByAes(str)
-	//打印
-	fmt.Printf(" 加密：%v\n 解密：%s\n ",
-		str, str1,
-	)
+	startTime := time.Now()
+	//helpers.EncryptFile("qtest.txt","test")
+	DecryptFile("encryptFile_test", "qtest.txt")
+	fmt.Printf("耗时：%v", time.Since(startTime))
 }
