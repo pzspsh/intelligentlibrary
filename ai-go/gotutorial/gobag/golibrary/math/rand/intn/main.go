17cc71d01459908b956c66ab8e2c97735bd6bc75
admin <panzhongsheng>
2023-12-01 14:58:23 +0800

    update

--- ai-go/gotutorial/gobag/golibrary/math/rand/intn/main.go
+++ ai-go/gotutorial/gobag/golibrary/math/rand/intn/main.go
@@ -14,4 +14,7 @@ func main() {
 	fmt.Println(rand.Intn(100))
 	fmt.Println(rand.Intn(100))
 	fmt.Println(rand.Intn(100))
+	// 初始化随机数的资源库, 如果不执行这行, 不管运行多少次都返回同样的值
+	// rand.Seed(time.Now().UnixNano())  // 该功能已经弃用
+	fmt.Println("A number from 1-100", rand.Intn(81))
 }
