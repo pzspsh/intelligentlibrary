75d30906b1118e6013ba6d0f388dc26142561b68
admin <panzhongsheng>
2023-12-06 15:05:48 +0800

    update

--- ai-go/functionlibrary/asr/main.go
+++ ai-go/functionlibrary/asr/main.go
@@ -5,6 +5,115 @@
 */
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/liuxp0827/govpr"
+	"github.com/liuxp0827/govpr/log"
+	"github.com/liuxp0827/govpr/waveIO"
+)
+
+type engine struct {
+	vprEngine *govpr.VPREngine
+}
+
+func NewEngine(sampleRate, delSilRange int, ubmFile, userModelFile string) *engine {
+	vprengine, err := govpr.NewVPREngine(sampleRate, delSilRange, true, ubmFile, userModelFile)
+	if err != nil {
+		return nil
+	}
+	return &engine{
+		vprEngine: vprengine,
+	}
+}
+
+func (e *engine) DestroyEngine() {
+	e.vprEngine = nil
+}
+
+func (e *engine) TrainSpeech(buffers [][]byte) error {
+
+	var err error
+	count := len(buffers)
+	for i := 0; i < count; i++ {
+		err = e.vprEngine.AddTrainBuffer(buffers[i])
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+	}
+
+	defer e.vprEngine.ClearTrainBuffer()
+	defer e.vprEngine.ClearAllBuffer()
+
+	err = e.vprEngine.TrainModel()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func (e *engine) RecSpeech(buffer []byte) error {
+
+	err := e.vprEngine.AddVerifyBuffer(buffer)
+	defer e.vprEngine.ClearVerifyBuffer()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	err = e.vprEngine.VerifyModel()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	Score := e.vprEngine.GetScore()
+	log.Infof("vpr score: %f", Score)
+	return nil
+}
+
+func main() {
+	log.SetLevel(log.LevelDebug)
+
+	vprEngine := NewEngine(16000, 50, "../ubm/ubm", "model/test.dat")
+	trainlist := []string{
+		"wav/train/01_32468975.wav",
+		"wav/train/02_58769423.wav",
+		"wav/train/03_59682734.wav",
+		"wav/train/04_64958273.wav",
+		"wav/train/05_65432978.wav",
+	}
+
+	trainBuffer := make([][]byte, 0)
+
+	for _, file := range trainlist {
+		buf, err := loadWaveData(file)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		trainBuffer = append(trainBuffer, buf)
+	}
+
+	verifyBuffer, err := waveIO.WaveLoad("wav/verify/34986527.wav")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	vprEngine.TrainSpeech(trainBuffer)
+	vprEngine.RecSpeech(verifyBuffer)
+}
+
+func loadWaveData(file string) ([]byte, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	// remove .wav header info 44 bits
+	data = data[44:]
+	return data, nil
 }
---
+++ ai-go/functionlibrary/facerecognition/testrun/main.go
@@ -0,0 +1,33 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2023-12-06 14:35:57
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/Kagami/go-face"
+)
+
+const dataDir = "testdata"
+
+// testdata 目录下两个对应的文件夹目录
+const (
+	modelDir  = dataDir + "/models"
+	imagesDir = dataDir + "/images"
+)
+
+func main() {
+	fmt.Println("Face Recognition...")
+
+	// 初始化识别器
+	rec, err := face.NewRecognizer(modelDir)
+	if err != nil {
+		fmt.Println("Cannot INItialize recognizer")
+	}
+	defer rec.Close()
+
+	fmt.Println("Recognizer Initialized")
+}
