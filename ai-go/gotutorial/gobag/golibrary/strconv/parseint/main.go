71ad81c58c96719ed3a9ae980705202a71c72a21
admin <panzhongsheng>
2023-12-06 17:50:16 +0800

    update

--- ai-go/gotutorial/gobag/golibrary/strconv/parseint/main.go
+++ ai-go/gotutorial/gobag/golibrary/strconv/parseint/main.go
@@ -10,20 +10,23 @@ import (
 	"strconv"
 )
 
+/*
+ParseInt解释给定基数(0,2到36)和位大小(0到64)的字符串s，并返回相应的值i。
+*/
 func main() {
 	v32 := "-354634382"
 	if s, err := strconv.ParseInt(v32, 10, 32); err == nil {
-		fmt.Printf("%T, %v\n", s, s)
+		fmt.Printf("10v32: %T, %v\n", s, s)
 	}
 	if s, err := strconv.ParseInt(v32, 16, 32); err == nil {
-		fmt.Printf("%T, %v\n", s, s)
+		fmt.Printf("16v32: %T, %v\n", s, s)
 	}
 
 	v64 := "-3546343826724305832"
 	if s, err := strconv.ParseInt(v64, 10, 64); err == nil {
-		fmt.Printf("%T, %v\n", s, s)
+		fmt.Printf("10v64: %T, %v\n", s, s)
 	}
 	if s, err := strconv.ParseInt(v64, 16, 64); err == nil {
-		fmt.Printf("%T, %v\n", s, s)
+		fmt.Printf("16v64: %T, %v\n", s, s)
 	}
 }
