1baf7cfe7019ff6babecc56cdc846c38c482a921
admin <panzhongsheng>
2023-12-22 18:12:14 +0800

    update

--- ai-go/functionlibrary/logging/gologger/logger_test.go
+++ ai-go/functionlibrary/logging/gologger/logger_test.go
@@ -3,16 +3,19 @@
 @Author : pan
 @Time   : 2023-10-25 16:17:25
 */
-package logger
+package logger_test
 
 import (
+	logger "function/logging/gologger"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
-	log := LoggerSet(`..\data\log\log1.txt`, 10, "10mb")
-	log2 := LoggerSet(`..\data\log\log2.txt`, 10, "10mb")
-	Info("hello world")
-	log.Error("你好！！！！！！！")
-	log2.Error("你好！！！！！！！")
+	logger.Logger(`..\data\log\log.txt`, 10, "mb")
+	log1 := logger.LoggerSet(`..\data\log\log1.txt`, 10, "10mb")
+	log2 := logger.LoggerSet(`..\data\log\log2.txt`, 10, "10mb")
+	logger.Info("logger hello world")
+	logger.Info("log hello world")
+	log1.Error("log1 你好！！！！！！！")
+	log2.Error("log2 你好！！！！！！！")
 }
