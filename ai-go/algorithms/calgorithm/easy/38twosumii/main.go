83bdbaa7b31d991f5930dd8934a9c4f5678e1e51
pzspsh <3277440871@qq.com>
2024-02-21 14:11:18 +0800

    update aigorithms

---
+++ ai-go/algorithms/calgorithm/easy/38twosumii/main.go
@@ -0,0 +1,10 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2024-02-21 13:44:02
+*/
+package main
+
+func main() {
+	// 两数之和
+}
---
+++ ai-go/algorithms/calgorithm/easy/42trailingzeroes/main.go
@@ -0,0 +1,10 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2024-02-21 13:48:43
+*/
+package main
+
+func main() {
+	// 阶乘后的零
+}
