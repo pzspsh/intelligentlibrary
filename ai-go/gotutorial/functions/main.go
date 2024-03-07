4594d4a9b4ed7a3c182205a381aa8b2d288ffd2c
pzspsh <3277440871@qq.com>
2024-03-07 15:09:56 +0800

    update

---
+++ ai-go/gotutorial/functions/main.go
@@ -0,0 +1,18 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2024-03-07 14:57:45
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello func")
+}
+
+func FuncDemo() {
+	fmt.Println("Func Demo")
+}
