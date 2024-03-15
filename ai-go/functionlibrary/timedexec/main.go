8847f6aa910f1fc160b665fa2bc08307ee00f27e
pzspsh <3277440871@qq.com>
2024-03-15 15:07:57 +0800

    update

--- ai-go/functionlibrary/timedexec/main.go
+++ ai-go/functionlibrary/timedexec/main.go
@@ -12,23 +12,15 @@ import (
 
 func scheduleTask() {
 	hour := 14   // 设置要执行任务的时间点，这里是14点
-	minute := 57 //
-	second := 0  //
-
+	minute := 57 // 设置要执行任务的时间，这里是每天的14点57分
+	second := 0
 	for { // 主循环，持续检查并执行任务
-		// 设置要执行任务的时间，这里是每天的14点30分
 		targetTime := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), hour, minute, second, 0, time.Local)
-
-		// 如果当前时间已经超过了目标时间，则设置目标时间为明天的14点30分
-		if time.Now().After(targetTime) {
+		if time.Now().After(targetTime) { // 如果当前时间已经超过了目标时间，则设置目标时间为明天的14点30分
 			targetTime = targetTime.AddDate(0, 0, 1)
 		}
-
-		// 等待到目标时间
-		time.Sleep(time.Until(targetTime))
-
-		// 执行任务
-		fmt.Println("现在是14点30分，执行任务！")
+		time.Sleep(time.Until(targetTime)) // 等待到目标时间
+		fmt.Println("现在是14点30分，执行任务！")     // 执行任务
 	}
 }
 
