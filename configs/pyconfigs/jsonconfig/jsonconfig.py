a95f1436af9a31004c6f71c4895c0ef565678d5a
pzspsh <3277440871@qq.com>
2024-05-22 17:19:43 +0800

    update

--- configs/pyconfigs/jsonconfig/jsonconfig.py
+++ configs/pyconfigs/jsonconfig/jsonconfig.py
@@ -1,6 +1,18 @@
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File   : jsonconfig.py
 @Time   : 2023-06-09 16:56:24
 @Author : pan
-'''
+"""
+import json
+
+
+def ReadJson(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    print(data)
+
+
+if __name__ == "__main__":
+    filename = "path/configs/config.json"
+    ReadJson(filename)
