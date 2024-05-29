23e59ffa4dda01070e668dda29aeca2a9e31615c
pzspsh <3277440871@qq.com>
2024-05-29 14:32:44 +0800

    update

--- configs/pyconfigs/jsonconfig/jsonconfig.py
+++ configs/pyconfigs/jsonconfig/jsonconfig.py
@@ -13,6 +13,11 @@ def ReadJson(filename):
     print(data)
 
 
+def WriteJson(filepath: str, data: dict):
+    with open(filepath, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     filename = "path/configs/config.json"
     ReadJson(filename)
