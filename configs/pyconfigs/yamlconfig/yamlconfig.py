4d7b2a334fe9664980a68ddaba432d8e1d51fc18
pzspsh <3277440871@qq.com>
2024-05-29 14:32:44 +0800

    update

--- configs/pyconfigs/yamlconfig/yamlconfig.py
+++ configs/pyconfigs/yamlconfig/yamlconfig.py
@@ -4,3 +4,21 @@
 @Time   : 2023-06-09 17:01:01
 @Author : pan
 '''
+import yaml
+
+def readyaml(filepath):
+    try:
+        stream = open(filepath, mode="r", encoding="utf-8")
+        mode = yaml.load(stream=stream, Loader=yaml.SafeLoader)
+        return mode
+    except Exception as err:
+        return err
+
+def WriteYaml(filepath, data):
+    with open(file=filepath, mode="w", encoding="utf-8") as f:
+        yaml.dump(data, f)
+
+if __name__ == "__main__":
+    filepath = "./pocs/example/test_yaml.yaml"
+    result = readyaml(filepath)
+    print(result)
