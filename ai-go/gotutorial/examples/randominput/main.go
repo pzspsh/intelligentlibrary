a2d9c6c3a4e512127e9056c8a3839b8c6e005e13
pzspsh <3277440871@qq.com>
2024-08-05 15:39:17 +0800

    update url parse

--- ai-go/gotutorial/examples/randominput/main.go
+++ ai-go/gotutorial/examples/randominput/main.go
@@ -1,3 +1,8 @@
+/*
+@File   : main.go
+@Author : pan
+@Time   : 2024-08-05 10:32:54
+*/
 package main
 
 import (
@@ -5,12 +10,22 @@ import (
 )
 
 func main() {
-	InputExec(3)
+	InputExec(5)
 	// 随机输入一个数，for循环一个总量数组，第0个和输入这个数的倍数更换值
 }
 
 func InputExec(input int) {
+	count := 0
+	value := ""
 	for i := range 100 {
-		fmt.Println(i)
+		if count%input == 0 {
+			value = getnewvalue(fmt.Sprintf("%v", count))
+		}
+		fmt.Println(value+": ", i)
+		count += 1
 	}
+}
+
+func getnewvalue(url string) string {
+	return url + "new value"
 }
