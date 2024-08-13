537121b1208c61e992f3af8c5cd0c1dc16b4e6c0
pzspsh <3277440871@qq.com>
2024-08-13 09:46:50 +0800

    update urlparse

--- ai-go/gotutorial/golibrary/runtime/example/main.go
+++ ai-go/gotutorial/golibrary/runtime/example/main.go
@@ -4,3 +4,24 @@
 @Time   : 2023-12-02 21:46:33
 */
 package main
+
+// 判断操作系统
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("操作系统是 macOS。")
+	case "linux":
+		fmt.Println("操作系统是 Linux。")
+	case "windows":
+		fmt.Println("操作系统是 Windows。")
+	default:
+		// 如果不是上面列出的系统，则打印未知
+		fmt.Printf("未知的操作系统: %s\n", os)
+	}
+}
