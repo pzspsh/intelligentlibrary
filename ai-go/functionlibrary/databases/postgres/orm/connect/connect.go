6e591370f4288f9bd65199e84b462eb6083a7764
pzspsh <3277440871@qq.com>
2024-09-14 17:45:59 +0800

    update gorm conn set

--- ai-go/functionlibrary/databases/postgres/orm/connect/connect.go
+++ ai-go/functionlibrary/databases/postgres/orm/connect/connect.go
@@ -8,6 +8,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -23,11 +24,18 @@ type DBConfig struct {
 
 func (c *DBConfig) PostgresConn() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Shanghai", c.Host, c.Username, c.Password, c.Database, c.Port)
+	// 获取通用数据库对象 sql.DB ，然后使用其提供的功能
 	sqlDB, err := sql.Open("pgx", dsn)
 	if err != nil {
 		fmt.Printf("sql open err:%v", err)
 		return nil, err
 	}
+	// SetMaxIdleConns 用于设置连接池中空闲连接的最大数量。
+	sqlDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		Conn:                 sqlDB,
 		PreferSimpleProtocol: true,
