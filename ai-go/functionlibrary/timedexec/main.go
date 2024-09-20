cbd8adead60bca94a8c55789d26a5fe7584baabe
pzspsh <3277440871@qq.com>
2024-09-20 15:30:03 +0800

    update add timer

--- ai-go/functionlibrary/timedexec/main.go
+++ ai-go/functionlibrary/timedexec/main.go
@@ -10,9 +10,20 @@ import (
 	"time"
 )
 
-func scheduleTask() {
-	hour := 15   // 设置要执行任务的时间点，这里是14点
-	minute := 10 // 设置要执行任务的时间，这里是每天的14点57分
+type TimeOption struct {
+	Mode   string // daily、weekly、monthly、yearly
+	Year   string
+	Month  string
+	Day    string
+	Hour   string
+	Minute string
+	Second string
+}
+
+// 定时器、定时执行
+func ScheduleTaskDemo() {
+	hour := 10   // 设置要执行任务的时间点，这里是14点
+	minute := 46 // 设置要执行任务的时间，这里是每天的14点57分
 	second := 0
 	for { // 主循环，持续检查并执行任务
 		targetTime := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), hour, minute, second, 0, time.Local)
@@ -24,6 +35,149 @@ func scheduleTask() {
 	}
 }
 
+func ScheduleTask(interval string, taskTime string, task func()) {
+
+}
+
+func SecondsTimer() { // 秒钟计时器
+	for {
+		time.Sleep(5 * time.Second)
+		fmt.Println("五秒钟执行一次")
+	}
+
+}
+
+func MinutesTimer() { // 分钟计时器
+	for {
+		time.Sleep(5 * time.Minute)
+		fmt.Println("五分钟执行一次")
+	}
+}
+
+func HourTimer() { // 小时计时器
+	for {
+		time.Sleep(5 * time.Hour)
+		fmt.Println("五个小时执行一次")
+	}
+}
+
+func DailyTimer() { // 每天计时器
+	hour := 18
+	minute := 3
+	second := 0
+	for {
+		targetTime := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), hour, minute, second, 0, time.Local)
+		if time.Now().After(targetTime) {
+			targetTime = targetTime.AddDate(0, 0, 1)
+		}
+		time.Sleep(time.Until(targetTime))
+		fmt.Printf("现在是%v点%v分，执行任务！\n", hour, minute)
+	}
+}
+
+func WeeklyTimer() { // 每周计时器
+	hour := 13
+	minute := 48
+	second := 0
+	mode := "Friday"
+	for {
+		weekday := ParseWeeklyTime(mode)
+		days := int(weekday - time.Now().Weekday())
+		if days <= 0 {
+			days += 7
+		}
+		targetTime := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), hour, minute, second, 0, time.Local)
+		if time.Now().After(targetTime) {
+			targetTime = targetTime.AddDate(0, 0, days)
+		}
+		time.Sleep(time.Until(targetTime))
+		fmt.Printf("现在是%v点%v分，执行任务！\n", hour, minute)
+	}
+}
+
+func MonthlyTimer() { // 每月计时器
+	hour := 14
+	minute := 30
+	second := 0
+	for {
+		targetTime := time.Date(time.Now().Year(), time.Now().Month(), 19, hour, minute, second, 0, time.Local)
+		if time.Now().After(targetTime) {
+			targetTime = targetTime.AddDate(0, 1, 0)
+			fmt.Println("AAAAAAA:", targetTime)
+		}
+		time.Sleep(time.Until(targetTime))
+		fmt.Printf("现在是%v点%v分，执行任务！\n", hour, minute)
+	}
+}
+
+func YearlyTimer() { // 每月计时器
+	month := 1
+	day := 2
+	hour := 11
+	minute := 45
+	second := 0
+	for {
+		targetTime := time.Date(time.Now().Year(), time.Month(month), day, hour, minute, second, 0, time.Local)
+		if time.Now().After(targetTime) {
+			targetTime = targetTime.AddDate(1, 0, 0)
+			fmt.Println("AAAAAAA:", targetTime)
+		}
+		time.Sleep(time.Until(targetTime))
+		fmt.Printf("现在是%v点%v分，执行任务！\n", hour, minute)
+	}
+}
+
+func ParseDailyTime() {
+
+}
+
+func ParseWeeklyTime(dayName string) time.Weekday {
+	switch dayName {
+	case "Sunday":
+		return time.Sunday
+	case "Monday":
+		return time.Monday
+	case "Tuesday":
+		return time.Tuesday
+	case "Wednesday":
+		return time.Wednesday
+	case "Thursday":
+		return time.Thursday
+	case "Friday":
+		return time.Friday
+	case "Saturday":
+		return time.Saturday
+	default:
+		return time.Sunday
+	}
+}
+
+func ParseMonthlyTime() {
+
+}
+
+func ParseYearlyTime() {
+
+}
+
 func main() {
-	scheduleTask()
+	// var month int
+	// var day int
+	// t := "01-03 10:11:00"
+	// fmt.Sscanf(t, "%02d-%02d %s", &month, &day, &t)
+	// fmt.Println(month, day)
+	// var day int
+	// t := "01-02 15:04"
+	// fmt.Sscanf(t, "%d %s", &day, &t)
+	// fmt.Println(day)
+	// var dayName string
+	// t := "Monday"
+	// fmt.Sscanf(t, "%s %s", &dayName, &t)
+	// weekDay := parseWeekDay(dayName)
+	// fmt.Println("BBBBBBBBB", int(weekDay))
+	// ScheduleTask()
+	// DailyTimer()
+	// WeeklyTimer()
+	// MonthlyTimer()
+	YearlyTimer()
 }
