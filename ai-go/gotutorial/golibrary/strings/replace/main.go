97deafdddee18e94b5856abb5a79e648dab135db
pzspsh <3277440871@qq.com>
2025-02-19 14:59:47 +0800

    update

--- ai-go/gotutorial/golibrary/strings/replace/main.go
+++ ai-go/gotutorial/golibrary/strings/replace/main.go
@@ -13,4 +13,5 @@ import (
 func main() {
 	fmt.Println(strings.Replace("oink oink oink", "k", "ky", 2))
 	fmt.Println(strings.Replace("oink oink oink", "oink", "moo", -1))
+	fmt.Println(strings.ReplaceAll("oink oink oink", "oink", "moo"))
 }
